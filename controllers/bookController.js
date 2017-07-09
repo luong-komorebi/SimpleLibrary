5b037eb500cb9db1a31cbd7a0d4d856810bd2bc4
Luong Vo <vo.tran.thanh.luong@gmail.com>
2017-07-10 00:52:36 +0700

    Index page working with library status

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,7 +1,31 @@
 var Book = require('../models/book');
+var Author = require('../models/author');
+var Genre = require('../models/genre');
+var BookInstance = require('../models/bookinstance');
+
+var async = require('async');
+
 
 exports.index = function (req, res) {
-  res.send('NOT IMPLEMENTED: Site Home Page');
+  async.parallel({
+    book_count: function (callback) {
+      Book.count(callback);
+    },
+    book_instance_count: function (callback) {
+      BookInstance.count(callback);
+    },
+    book_instance_available_count: function (callback) {
+      BookInstance.count({ status: 'available'}, callback);
+    },
+    author_count: function (callback) {
+      Author.count(callback);
+    },
+    genre_count: function (callback) {
+      Genre.count(callback);
+    },
+  }, function (err, results) {
+    res.render('index', { title: 'Local Library Home', error: err, data: results })
+  });
 };
 
 // Display list of all books
