ad8d85f7c4c4b760cb2135f99f96a0d41192fb5c
Luong Vo <vo.tran.thanh.luong@gmail.com>
2017-07-10 23:07:46 +0700

    Add author_add form

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -31,13 +31,45 @@ exports.author_detail = function (req, res, next) {
 };
 
 // Display Author create form on GET
-exports.author_create_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: Author create GET');
+exports.author_create_get = function (req, res, next) {
+  res.render('author_form', { title: 'Create Author' });
 };
 
 // Handle Author create on POST
-exports.author_create_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: Author create POST');
+exports.author_create_post = function (req, res, next) {
+  req.checkBody('first_name', 'First name must be specified.').notEmpty(); //We won't force Alphanumeric, because people might have spaces.
+  req.checkBody('family_name', 'Family name must be specified.').notEmpty();
+  req.checkBody('family_name', 'Family name must be alphanumeric text.').isAlpha();
+  req.checkBody('date_of_birth', 'Invalid date').optional({ checkFalsy: true }).isDate();
+  req.checkBody('date_of_death', 'Invalid date').optional({ checkFalsy: true }).isDate();
+
+  req.sanitize('first_name').escape();
+  req.sanitize('family_name').escape();
+  req.sanitize('first_name').trim();
+  req.sanitize('family_name').trim();
+  req.sanitize('date_of_birth').toDate();
+  req.sanitize('date_of_death').toDate();
+
+  var errors = req.validationErrors();
+
+  var author = new Author(
+    {
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death
+  });
+
+  if (errors) {
+    res.render('author_form', { title: 'Create Author', author : author, errors: errors });
+    return;
+  }
+  else {
+    author.save(function (err) {
+      if (err) return next(err);
+      res.redirect(author.url);
+    });
+  }
 };
 
 // Display Author delete form on GET
