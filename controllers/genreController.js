b430cfe049f19780eb118c2293d0424cc3e54707
Luong Vo <vo.tran.thanh.luong@gmail.com>
2017-07-10 11:15:18 +0700

    Add genre detail page

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,4 +1,6 @@
 var Genre = require('../models/genre');
+var Book = require('../models/book');
+var async = require('async');
 
 // Display list of all Genre
 exports.genre_list = function (req, res) {
@@ -8,12 +10,26 @@ exports.genre_list = function (req, res) {
       if (err) { return next(err); }
       // successful rendering
       res.render('genre_list', { title: 'Genre List', list_genres:  list_genres});
-    })
+    });
 };
 
 // Display detail page for a specific Genre
-exports.genre_detail = function (req, res) {
-  res.send('NOT IMPLEMENTED: Genre detail: ' + req.params.id);
+exports.genre_detail = function (req, res, next) {
+  async.parallel({
+    genre: function(callback) {
+      Genre.findById(req.params.id)
+        .exec(callback);
+    },
+
+    genre_books: function(callback) {
+      Book.find({ 'genre': req.params.id})
+        .exec(callback);
+    },
+  }, function(err, results) {
+    if (err) return next(err);
+    // successful rendering
+    res.render('genre_detail', { title: 'Genre Detail', genre: results.genre, genre_books: results.genre_books });
+  });
 };
 
 // Display Genre create form on GET
