d92dd9547dd91ea3ea60ca23cf21e35be73cd57b
Luong Vo <vo.tran.thanh.luong@gmail.com>
2017-07-10 11:51:10 +0700

    Add author detail page

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,4 +1,7 @@
 var Author = require('../models/author');
+var Book = require('../models/book');
+
+var async = require('async');
 
 // Display author list
 exports.author_list = function (req, res, next) {
@@ -11,8 +14,20 @@ exports.author_list = function (req, res, next) {
 };
 
 // Display detail page for a specific Author
-exports.author_detail = function (req, res) {
-  res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
+exports.author_detail = function (req, res, next) {
+  async.parallel({
+    author: function(callback) {
+      Author.findById(req.params.id)
+        .exec(callback);
+    },
+    author_books: function(callback) {
+      Book.find({ 'author': req.params.id}, 'title summary')
+        .exec(callback);
+    },
+  }, function (err, results) {
+    if (err) return next(err);
+    res.render('author_detail', { title: 'Author Detail', author: results.author, author_books: results.author_books});
+  });
 };
 
 // Display Author create form on GET
